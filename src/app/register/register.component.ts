8614f884972d50ad260c259948c81c3015ae5f81
cangui <canguijc@gmail.com>
2021-04-22 12:32:31 +0200

    add/remove favorie lors de l ajout d'un amis

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../_services/auth.service";
+import {TokenStorageService} from "../_services/token-storage.service";
 
 @Component({
   selector: 'app-register',
@@ -20,26 +21,62 @@ export class RegisterComponent implements OnInit {
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
+  private currentUser: any;
+  private favorieId: any;
+  private data: unknown[] | undefined;
+  private speudo: any;
 
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,private token: TokenStorageService) { }
 
   ngOnInit(): void {
+    this.currentUser = this.token.getUser();
   }
 
   onSubmit(): void {
-    const { username, email, password,age,famille,races,nourriture } = this.form;
+    const {username, email, password, age, famille, races, nourriture} = this.form;
     console.log(this.form);
-    this.authService.register(username, email, password,age,famille,races,nourriture).subscribe(
-      data => {
-        console.log(data);
-        this.isSuccessful = true;
-        this.isSignUpFailed = false;
-      },
-      err => {
-        this.errorMessage = err.error.message;
-        this.isSignUpFailed = true;
-      }
-    );
+    if (!this.currentUser) {
+      this.authService.register(username, email, password, age, famille, races, nourriture).subscribe(
+        data => {
+          console.log(data);
+          this.isSuccessful = true;
+          this.isSignUpFailed = false;
+        },
+        err => {
+          this.errorMessage = err.error.message;
+          this.isSignUpFailed = true;
+        }
+      );
+    } else if (this.currentUser) {
+      console.log("dans else if")
+      this.authService.register(username, email, password, age, famille, races, nourriture).subscribe(
+        data => {
+          console.log(data);
+          this.favorieId=data
+          this.isSuccessful = true;
+          this.isSignUpFailed = false;
+          this.addFavorie(data);
+        },
+        err => {
+          this.errorMessage = err.error.message;
+          this.isSignUpFailed = true;
+        }
+      );
+
+    }
+
+    }
+  addFavorie(favorieId:any){
+
+    this.authService.updateFavorie(this.currentUser.id,favorieId)
+      .subscribe(val=>{
+          console.log(val);
+        }
+      )
+
+
   }
+
+
 }
